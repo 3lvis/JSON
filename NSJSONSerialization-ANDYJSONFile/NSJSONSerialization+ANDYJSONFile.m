4be8e687b2109297e07ef1b93eb8dc79911dc658
Elvis Nuñez <NSElvis@users.noreply.github.com>
2014-12-01 17:09:39 +0100

    Remove headers

--- NSJSONSerialization-ANDYJSONFile/NSJSONSerialization+ANDYJSONFile.m
+++ NSJSONSerialization-ANDYJSONFile/NSJSONSerialization+ANDYJSONFile.m
@@ -1,10 +1,3 @@
-//
-//  NSJSONSerialization+ANDYJSONFile.m
-//
-//  Created by Elvis Nunez on 4/29/14.
-//  Copyright (c) 2014 Elvis Nuñez. All rights reserved.
-//
-
 #import "NSJSONSerialization+ANDYJSONFile.h"
 
 @implementation NSJSONSerialization (ANDYJSONFile)
