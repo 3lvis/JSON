cc9f977aaf4409f8a549c944ff726ed7d8aee776
Elvis Nuñez <elvisnunez@me.com>
2015-05-02 08:25:41 +0200

    Add pretty print

--- Pod/Tests/Tests.swift
+++ Pod/Tests/Tests.swift
@@ -3,7 +3,18 @@ import XCTest
 
 class Tests: XCTestCase {
   func testFromFileNamed() {
-    let result = JSON.from("simple.json", bundle: NSBundle(forClass: self.classForKeyedArchiver!))
-    XCTAssertNotNil(result.result)
+    let (result, error) = JSON.from("simple.json", bundle: NSBundle(forClass: self.classForKeyedArchiver!))
+    XCTAssertNotNil(result)
+  }
+
+  func testPrettyPrint() {
+    let (result, error) = JSON.from("simple.json", bundle: NSBundle(forClass: self.classForKeyedArchiver!))
+    var success = false
+    if let pretty = result?.prettyDescription() {
+      XCTAssertEqual("[\n  {\n    \"id\" : 1,\n    \"name\" : \"Hi\"\n  }\n]", pretty)
+      success = true
+    }
+
+    XCTAssertTrue(success)
   }
 }
--- Source/JSON.swift
+++ Source/JSON.swift
@@ -1,19 +1,37 @@
 import Foundation
 
 class JSON {
-  class func from(fileName: String) -> (result: AnyObject?, error: NSError?) {
+  class func from(fileName: String) -> (result: NSObject?, error: NSError?) {
     return from(fileName, bundle: NSBundle.mainBundle())
   }
 
-  class func from(fileName: String, bundle: NSBundle) -> (result: AnyObject?, error: NSError?) {
+  class func from(fileName: String, bundle: NSBundle) -> (result: NSObject?, error: NSError?) {
     var error: NSError?
-    var result: AnyObject?
+    var result: NSObject?
     if let filePath = bundle.pathForResource(fileName.stringByDeletingPathExtension, ofType: fileName.pathExtension) {
       if let data: NSData = NSData(contentsOfFile: filePath) {
-        result = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &error)
+        result = NSJSONSerialization.JSONObjectWithData(data, options: .allZeros, error: &error) as? NSObject
       }
     }
 
     return (result, error)
+  }
+}
+
+extension NSObject {
+  func prettyDescription() -> String? {
+    var pretty: String?
+    var error: NSError?
+    if let data = NSJSONSerialization.dataWithJSONObject(self, options: .PrettyPrinted, error: &error) {
+      pretty = NSString(data: data, encoding: NSUTF8StringEncoding) as? String
+    }
+
+    return pretty
+  }
+
+  func prettyPrint() {
+    if let pretty = self.prettyDescription() {
+      println(pretty)
+    }
   }
 }
